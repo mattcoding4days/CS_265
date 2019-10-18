9807c31e7463d8aa180c171d4aacb15417c95af5
Matt Williams <mattinzhuhai@gmail.com>
2019-10-18 01:44:05 -0700

    Test compiling the baseSchema class after including in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
+#include "base.cpp"
+#include <vector>
 
+
+
+using namespace GraderProgram;
+
+int main ()
+{
+   BaseSchema baseStructure;
+   std::vector<BaseSchema> baseContainer;
+
+
+   return 0;
+}
