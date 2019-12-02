bcb1a242e847b13d9af28a1e4af17a739628cc70
Matt Williams <matt_k_williams@zoho.com>
2019-12-01 18:13:26 -0800

    restarting

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,8 @@
-#include "student.cpp"
-#include "base.cpp"
-#include <vector>
-
-using namespace GraderApplication;
-using stuContainer = std::vector<StudentData>;
+#include <iostream>
 
 
-const std::string DATA = "../test/official.txt";
 
 int main()
 {
-   int amount = 4;
-   stuContainer s;
-   s.reserve(amount);
-   
-   for ( int i = 0; i < amount; ++i ) {
-      StudentData student;
-      student.loadDataFile(DATA);
-      s.emplace_back(student);
-   }
-
-   std::cout << "\n\n";
-   for ( const auto &itr: s ) {
-      itr.printStudentObject();
-   }
    return 0;
 }
