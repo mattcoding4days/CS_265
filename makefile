afc17abb14f456da44660f4cabb0a0e2a3b28c3b
Matt Williams <mattinzhuhai@gmail.com>
2019-12-03 03:32:04 -0800

    something

---
+++ makefile
@@ -0,0 +1,7 @@
+CXX="g++ -Wall -Wextra"
+BINARY="bin/"
+RELEASE_BUILD="obj/"
+DEBUG_BUILD="objd/"
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <string.h>
 
 
+struct CliOptions {
 
-int main()
+};
+
+void printUsage();
+int getOption(int count, char **argv, const char *accepted);
+
+
+/* Init main routine */
+int main(int argc, char *argv[])
 {
+   if (argc < 2) {
+      printUsage();
+   }
+   const char *sname = "-s";
+   int status;
+   status = getOption(argc, argv, sname);
+
+   if (status) {
+      
+   }
+
    return 0;
+}
+
+void printUsage()
+{
+   std::cout << "Usage: grader -s <path/to/file>" << std::endl;
+   exit(2);
+}
+
+/* get user option, :*/
+int getOption(int count, char **argv, const char *sname)
+{
+   for (int i = 1; i < count; ++i) {
+      const char *temp = argv[i];
+      if (strcmp(sname, temp) == 0) {
+         fprintf(stdout, "Found s flag: %s\n", temp);
+         return 0;
+      } else {
+         
+      }
+   }
+   return -1;
 }
